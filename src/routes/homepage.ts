00978d165e0c34d00caa49c9adcbb77255ee8535
Ekene Madu <EkeneMadu@protonmail.com>
2019-04-22 21:46:48 +0000

    Clean up language

--- src/routes/homepage.ts
+++ src/routes/homepage.ts
@@ -5,5 +5,5 @@ let reqCount = 0;
 
 export default function(): string {
   const uptime = Date.now() - startTime;
-  return `This is an API server. Please make requests against a proper endpoint. This server uptime: ${uptime}ms and this page has been requested ${++reqCount} times`
+  return `This is an API server. Please make requests against an endpoint. This server uptime: ${uptime}ms and this page has been requested ${++reqCount} times`
 }
