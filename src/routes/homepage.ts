39e77c67515ee1b2315a85a92103e78e217b40e5
Ekene Madu <EkeneMadu@protonmail.com>
2019-04-23 03:26:30 +0000

    Add ticks

--- src/routes/homepage.ts
+++ src/routes/homepage.ts
@@ -2,8 +2,11 @@
 let startTime = Date.now();
 let reqCount = 0;
 
+let seconds = 0;
+setInterval(() => seconds++, 1000);
+
 
 export default function(): string {
   const uptime = Date.now() - startTime;
-  return `This is an API server. Please make requests against an endpoint. This server uptime: ${uptime}ms and this page has been requested ${++reqCount} times`
+  return `This is an API server. Please make requests against an endpoint. This server uptime: ${(uptime/1000).toFixed(2)}s and ${seconds} ticks and this page has been requested ${++reqCount} times`
 }
