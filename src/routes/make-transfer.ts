55c51e928af6671f83af761c2267f4b18030776a
Ekene Madu <EkeneMadu@protonmail.com>
2019-05-26 22:48:28 +0000

    Fix makeTransfer

--- src/routes/make-transfer.ts
+++ src/routes/make-transfer.ts
@@ -4,32 +4,37 @@ import { withTransaction } from '../db/util';
 import * as dbTransfer from '../db/transfer';
 import * as rpcClient from '../util/rpc-client';
 
+// expects a { transfer, hookout }
 export default async function makeTransfer(body: any): Promise<string> {
-  const transfer = hi.BitcoinTransfer.fromPOD(body);
+  if (typeof body !== 'object') {
+    throw 'expected object of {transfer,hookout}';
+  }
+
+  const transfer = hi.Transfer.fromPOD(body.transfer);
   if (transfer instanceof Error) {
     throw transfer;
   }
-
-  if (!transfer.isValid()) {
-    throw 'INVALID_TRANSFER';
+  
+  if (!transfer.isAuthorized()) {
+    throw 'transfer was not authorized';
   }
 
-  const hookout = transfer.output;
+  const hookout = hi.Hookout.fromPOD(body.hookout);
+  if (hookout instanceof Error) {
+    throw hookout;
+  }
 
-  const actualFee = transfer.fee();
+  const actualFee = transfer.inputAmount() - (transfer.change.amount + hookout.amount);
   const feeRate = actualFee / hi.Params.templateTransactionWeight;
 
   if (feeRate < 0.25) {
     throw 'fee was ' + feeRate + ' but require a feerate of at least 0.25';
   }
 
-  const send = {
-    hookout,
-    transaction: await rpcClient.createTransaction(hookout.bitcoinAddress, hookout.amount, feeRate),
-  };
+  const sendTransaction = await rpcClient.createTransaction(hookout.bitcoinAddress, hookout.amount, feeRate);
 
   const ackTransfer: hi.AcknowledgedTransfer = hi.Acknowledged.acknowledge(
-    transfer.prune(),
+    transfer,
     hi.Params.acknowledgementPrivateKey
   );
 
@@ -41,14 +46,13 @@ export default async function makeTransfer(body: any): Promise<string> {
     }
     assert.strictEqual(insertRes, 'SUCCESS');
 
-    await dbTransfer.insertTransactionHookout(dbClient, send.hookout, send.transaction);
+    await dbTransfer.insertTransactionHookout(dbClient, hookout, sendTransaction);
   });
 
-  if (send) {
-    rpcClient.sendRawTransaction(send.transaction.hex).catch(err => {
-      console.error('[INTERNAL_ERROR] [ACTION_REQUIRED] could not send transaction: ', send, ' got: ', err);
+    rpcClient.sendRawTransaction(sendTransaction.hex).catch(err => {
+      console.error('[INTERNAL_ERROR] [ACTION_REQUIRED] could not send transaction: ', sendTransaction, ' got: ', err);
     });
-  }
+  
 
   return ackTransfer.acknowledgement.toPOD();
 }
